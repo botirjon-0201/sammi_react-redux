39267aaede1376a87daca6509369a647e92a0f4d
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2022-12-28 18:17:28 +0500

    Loader was added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,64 +1,71 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Loader } from "../ui";
 
 function Main() {
-  const { articles } = useSelector((state) => state.article);
+  const { isLoading, articles } = useSelector((state) => state.article);
 
   return (
-    <div className="album py-5">
-      <div className="container">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          {articles.map((article) => (
-            <div className="col" key={article.id}>
-              <div className="card h-100 shadow-sm">
-                <svg
-                  className="bd-placeholder-img card-img-top"
-                  width="100%"
-                  height="225"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-label="Placeholder: Thumbnail"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                >
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595c"></rect>
-                </svg>
-                <div className="card-body">
-                  <p className="card-text fw-bold">{article.title}</p>
-                  <p className="card-text">{article.description}</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-outline-success"
-                      >
-                        View
-                      </button>
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-outline-secondary"
-                      >
-                        Edit
-                      </button>
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-outline-danger"
-                      >
-                        Delete
-                      </button>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="album py-5">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              {articles.map((article) => (
+                <div className="col" key={article.id}>
+                  <div className="card h-100 shadow-sm">
+                    <svg
+                      className="bd-placeholder-img card-img-top"
+                      width="100%"
+                      height="225"
+                      xmlns="http://www.w3.org/2000/svg"
+                      role="img"
+                      aria-label="Placeholder: Thumbnail"
+                      preserveAspectRatio="xMidYMid slice"
+                      focusable="false"
+                    >
+                      <title>Placeholder</title>
+                      <rect width="100%" height="100%" fill="#55595c"></rect>
+                    </svg>
+                    <div className="card-body">
+                      <h5 className="card-text fw-bold">{article.title}</h5>
+                      <p className="card-text">{article.description}</p>
                     </div>
-                    <small className="text-muted fw-bold text-capitalize">
-                      {article.author.username}
-                    </small>
+                    <div className="card-footer d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-success"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-danger"
+                        >
+                          Delete
+                        </button>
+                      </div>
+                      <small className="text-muted fw-bold text-capitalize">
+                        {article.author.username}
+                      </small>
+                    </div>
                   </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="spinner-border d-block mx-auto" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+}
+
+export default Loader;
